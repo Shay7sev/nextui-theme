48c982b996efd4716986f92b7177548e6246b9ea
shay7sev <shay7sev@gmail.com>
2023-12-10 21:09:58 +0800

    build(app): init

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: ["next/core-web-vitals"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  rules: {
+    // "no-console": "error",
+    // 'prettier/prettier': 'warn',
+    "no-var": "error", // 要求使用 let 或 const 而不是 var
+    eqeqeq: [2, "allow-null"], // 使用 === 替代 ==
+    "no-multiple-empty-lines": ["error", { max: 1 }], // 不允许多个空行
+    "no-multi-spaces": 2, // 不能用多余的空格
+    "no-trailing-spaces": 2, // 一行结束后面不要有空格
+    "no-use-before-define": "off", // 禁止在 函数/类/变量 定义之前使用它们
+    "prefer-const": "off", // 此规则旨在标记使用 let 关键字声明但在初始分配后从未重新分配的变量，要求使用 const
+    "no-irregular-whitespace": "off", // 禁止不规则的空白
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react-hooks/exhaustive-deps": "off",
+    "react/display-name": "off",
+    "import/no-anonymous-default-export": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ], // 禁止定义未使用的变量
+  },
+};
---
+++ app/util.ts
@@ -0,0 +1,121 @@
+import dom2element from "dom-to-image";
+
+/**
+ *
+ * @param el {HTMLElement}
+ * @return {Promise<unknown>}
+ */
+export function toCanvas(el: any) {
+  return new Promise((resolve) => {
+    dom2element.toPng(el).then((pngDataURI) => {
+      const canvas = document.createElement("canvas");
+      const rect = el.getBoundingClientRect();
+      canvas.style.position = "fixed";
+      canvas.style.left = rect.left + "px";
+      canvas.style.top = rect.top + "px";
+      canvas.style.zIndex = "40";
+      canvas.width = rect.width;
+      canvas.height = rect.height;
+      canvas.style.width = rect.width + "px";
+      canvas.style.height = rect.height + "px";
+      const context = canvas.getContext("2d");
+      const img = new Image();
+      img.onload = () => {
+        context?.drawImage(img, 0, 0);
+        setTimeout(() => resolve(canvas));
+      };
+      img.src = pngDataURI;
+    });
+  });
+}
+
+export function easeInOutQuint(
+  elapsed: number,
+  initialValue: number,
+  amountOfChange: number,
+  duration: number
+) {
+  if ((elapsed /= duration / 2) < 1) {
+    return (
+      (amountOfChange / 2) * elapsed * elapsed * elapsed * elapsed * elapsed +
+      initialValue
+    );
+  }
+  return (
+    (amountOfChange / 2) *
+      ((elapsed -= 2) * elapsed * elapsed * elapsed * elapsed + 2) +
+    initialValue
+  );
+}
+
+export function easeInOutQuart(
+  elapsed: number,
+  initialValue: number,
+  amountOfChange: number,
+  duration: number
+) {
+  if ((elapsed /= duration / 2) < 1) {
+    return (
+      (amountOfChange / 2) * elapsed * elapsed * elapsed * elapsed +
+      initialValue
+    );
+  }
+  return (
+    (-amountOfChange / 2) * ((elapsed -= 2) * elapsed * elapsed * elapsed - 2) +
+    initialValue
+  );
+}
+
+function getMousePos(canvas: any, evt: any) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: ((evt.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
+    y: ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
+  };
+}
+
+function getMaxRadius(canvas: any) {
+  return Math.sqrt(Math.pow(canvas.width, 2) + Math.pow(canvas.height, 2));
+}
+
+export const crop = (
+  canvas: any,
+  initialPosition: any,
+  { reverse = false }
+) => {
+  const ctx = canvas.getContext("2d");
+  const { x, y } = getMousePos(canvas, initialPosition);
+  const maxRadius = getMaxRadius(canvas);
+
+  return new Promise((resolve) => {
+    let progress = 0;
+    const duration = 60;
+    ctx.fillStyle = "rgba(255, 255, 255, 1)";
+    ctx.globalCompositeOperation = reverse
+      ? "destination-in"
+      : "destination-out";
+
+    function draw() {
+      let radius;
+      if (reverse) {
+        radius = easeInOutQuint(progress, maxRadius, -maxRadius, duration);
+      } else {
+        radius = easeInOutQuart(progress, 0, maxRadius, duration);
+      }
+
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+      ctx.fill();
+
+      progress++;
+
+      if (progress < duration) {
+        requestAnimationFrame(draw);
+      } else {
+        resolve(canvas);
+      }
+    }
+
+    draw();
+  });
+};
